d907a21153a08065b9dd03d995d59cbb0f5df1d8
vsarapin-developer <vsarapin.dev@gmail.com>
2023-10-07 01:44:06 +0300

    add some notes for datatables.net

--- resources/js/components/GlobalComponents/Notes.vue
+++ resources/js/components/GlobalComponents/Notes.vue
@@ -19,8 +19,8 @@
                         <code>
                             $apiToken = "6221023312:AAFIK-MmYO8aTvvTabPNNdRT2DsTAnn2bUU";<br>
                             $data = [<br>
-                                &emsp;&emsp;&emsp;&emsp;'chat_id' => '-1001930869363',<br>
-                                &emsp;&emsp;&emsp;&emsp;'text' => json_encode($quickReplies)<br>
+                                <span class="tab-2">'chat_id' => '-1001930869363',</span><br>
+                                <span class="tab-2">'text' => json_encode($quickReplies)</span><br>
                             ];<br>
                             $response = file_get_contents("https://api.telegram.org/bot$apiToken/sendMessage?" . http_build_query($data) );
                         </code>
@@ -41,6 +41,7 @@
                     <a class="text-body-2 blue-accent-4--text text--lighten-5" href="https://forum.unity.com/threads/how-do-i-make-an-aiming-reticle-that-follows-objects.905063/" target="_blank">How do I make an aiming reticle that follows objects? - Unity Forum</a><br><br>
                     <a class="text-body-2 blue-accent-4--text text--lighten-5" href="https://answers.unity.com/questions/1815284/how-to-check-if-player-is-inside-enemy-field-of-vi.html" target="_blank">How to check if player is inside enemy field of view without RenderTexture? </a><br><br>
                     <a class="text-body-2 blue-accent-4--text text--lighten-5" href="https://ecosoft.ua/ua/komplekt-kartridzhey-ecosoft-dlya-troynogo-filtra/" target="_blank">Ecosoft cartridge</a><br><br>
+                    <a class="text-body-2 blue-accent-4--text text--lighten-5" href="https://www.pcgamingwiki.com/wiki/Home" target="_blank">PCGamingWiki aims to list fixes and workarounds for every single PC game.</a><br><br>
                 </v-card-text>
             </v-card>
             <v-card  width="500" class="ml-5 mr5 mt-5">
@@ -51,6 +52,32 @@
                     <p  class="text-body-2"><code>redis-server --port 6379 --protected-mode no</code></p>
                     <p  class="text-body-2"><code>redis-cli -h localhost -p 6379</code></p>
                     <p  class="text-body-2"><code>CONFIG SET protected-mode no</code></p>
+                </v-card-text>
+            </v-card>
+            <v-card  width="500" class="ml-5 mr5 mt-5">
+                <v-card-title>
+                    DataTables.Net
+                </v-card-title>
+                <v-card-text>
+                    <span class="text-body-2">
+                        <code>
+                        let datatable = jQuery(this).DataTable();<br>
+                        console.log(datatable)<br>
+
+                        jQuery(document)<br>
+                            <span class="tab-1">.on('draw.dt', function() {</span><br>
+                            <span class="tab-3">setTimeout(() => {</span><br>
+                            <span class="tab-3">window.scrollTo({</span><br>
+                            <span class="tab-3">top: 100,</span><br>
+                            <span class="tab-3">left: 100,</span><br>
+                            <span class="tab-3">behavior: "smooth",</span><br>
+                            <span class="tab-2">});</span><br>
+                            <span class="tab-1">}, 10);</span><br>
+                        });<br><br>
+
+                        Please note that, as with all DataTables emitted events, this event is triggered with the dt namespace. As such, to listen for this event, you must also use the dt namespace by simply appending .dt to your event name, as shown in the example below.
+                        </code>
+                    </span>
                 </v-card-text>
             </v-card>
         </v-row>
@@ -64,5 +91,16 @@ export default {
 </script>
 
 <style scoped>
-
+.tab-1 {
+    display: inline-block;
+    margin-left: 20px;
+}
+.tab-2 {
+    display: inline-block;
+    margin-left: 40px;
+}
+.tab-3 {
+    display: inline-block;
+    margin-left: 60px;
+}
 </style>
